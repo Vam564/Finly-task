d2e1885295abfa48feaa0e23a0cf88968ede8780
Vam564 <64726014+Vam564@users.noreply.github.com>
2021-05-29 21:25:39 +0530

    Update NestedComments.js

--- Finly-task/src/components/Comments/NestedComments.js
+++ Finly-task/src/components/Comments/NestedComments.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const NestedComments = ({ data, setData }) => {
+const NestedComments = ({ data,nestedData,id, setData,main_data_index ,handleReply}) => {
 
     const [editFlag, setEditFlag] = useState(false)
     const [isEditComment, setIsEditComment] = useState('')
@@ -14,10 +14,12 @@ const NestedComments = ({ data, setData }) => {
     }
     const handleEdit = (index, d) => {
         setData([...data.map((x, i) => {
-            if (i == index) {
-
-                x.edit = true
-
+            if (x.id == id) {
+               x.nestedComments.map((y,i)=>{
+                   if(i == index){
+                       y.edit = true
+                   }
+               })
             }
             return x
         })])
@@ -25,39 +27,52 @@ const NestedComments = ({ data, setData }) => {
         setIsEditComment(d)
     }
     const handleDelete = (index, d) => {
-        let arr = [...data]
-        arr.splice(index, 1);
+       let arr = [...data]
+        arr[main_data_index].nestedComments.splice(index,1)
         setData(arr)
     }
-    const handleEditedComment = (value) => {
+    const handleEditedCommentSubmit = (value) => {
 
         setData([...data.map((x, i) => {
-            if (i == value) {
-
-
-                x.comment = isEditComment
-                x.edit = false
-
-            }
+            if (x.id == id) {
+                x.nestedComments.map((y,i)=>{
+                    if (i == value) {
+                        y.comment = isEditComment
+                        y.edit = false
+        
+                    }
+                })
+             }
+           
             return x
         })])
         setEditFlag(false)
 
     }
-    const handleReply = () => {
-
+    const handleLike = (value) =>{
+        setData([...data.map((x,i)=>{
+            if(x.id == id){
+                x.nestedComments.map((y,i)=>{
+                    if (i == value) {
+                        y.like = !y.like
+                    }
+                })
+            }
+            return x
+        })])
     }
     return (
         <div style={{marginLeft:"20px"}}>
             <ul>
                 {
 
-                    data && data.map((x, i) => (
+                    nestedData &&  nestedData.map((x, i) => (
                         x.edit ? (<>
-                            <input type="text" value={isEditComment} placeholder="edit your comment" onChange={handleEditComment} /><button onClick={() => handleEditedComment(i)}>Submit</button>
+                            <input type="text" value={isEditComment} placeholder="edit your comment" onChange={handleEditComment} /><button onClick={() => handleEditedCommentSubmit(i)}>Submit</button>
                         </>) : (
                             <>
-                                <li key={i}>{x.comment} <span onClick={() => handleEdit(i, x.comment)}>Edit</span> <span onClick={() => handleDelete(i, x.comment)}>Delete</span><span onClick={handleReply}>Reply</span></li>
+                                <small>{x.uname}</small><br></br>
+                                <li key={i}>{x.comment} <span onClick={() => handleEdit(i, x.comment)}>Edit</span> <span onClick={() => handleDelete(i, x.comment)}>Delete</span><span onClick={()=>handleLike(i)} style={{color:x.like ? 'green' : 'red'}}>{x.like ? "Liked" : "UnLiked"}</span><span className="reply" onClick={()=>handleReply(id,i,x.uname)}>Reply</span></li>
 
                             </>
                         )
