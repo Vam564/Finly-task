30f23892aa273d6f3cf6952e49ca6b025e0be594
Vam564 <64726014+Vam564@users.noreply.github.com>
2021-05-29 21:24:01 +0530

    Update index.js

--- Finly-task/src/index.js
+++ Finly-task/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ContextProvider } from './context/Context'
+
 
 ReactDOM.render(
-  <ContextProvider>
-   
       <App />,
-    
-  </ContextProvider>,
   document.getElementById('root')
 );
 
