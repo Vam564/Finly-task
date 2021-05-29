319cd2b07727b46d75945d10d334274ecad50198
Vam564 <64726014+Vam564@users.noreply.github.com>
2021-05-29 21:38:00 +0530

    Update Comment.js

--- Finly-task/src/components/Comments/Comment.js
+++ Finly-task/src/components/Comments/Comment.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import DisplayComment from './DisplayComment';
-import { Context } from '../../context/Context'
 
 import axios from 'axios'
 const Comment = () => {
