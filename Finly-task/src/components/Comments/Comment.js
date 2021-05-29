650aa31fd90c433f2d4a4744526f848f5fc33b5a
Vam564 <64726014+Vam564@users.noreply.github.com>
2021-05-29 21:24:37 +0530

    Update Comment.js

--- Finly-task/src/components/Comments/Comment.js
+++ Finly-task/src/components/Comments/Comment.js
@@ -1,44 +1,53 @@
-import React ,{useState, useEffect}from 'react'
+import React, { useState, useEffect } from 'react'
 import DisplayComment from './DisplayComment';
+import { Context } from '../../context/Context'
 
 import axios from 'axios'
 const Comment = () => {
     const [data, setData] = useState([])
     const [comment, setComment] = useState('')
-    const [count,setCount] = useState(0)
+    const [count, setCount] = useState(0)
     const [collapse, setCollapse] = useState(false)
 
 
     useEffect(() => {
         axios.get('https://jsonblob.com/api/fd64c808-c07c-11eb-a8c9-2f0c8cbd33b6').then((response) => {
-          console.log(response.data.Comments)
-          setData(response.data.Comments)
+            console.log(response.data.Comments)
+            setData(response.data.Comments)
         })
     }, [])
 
 
-    const handleComment = (e) =>{
-      
+    const handleComment = (e) => {
+
         console.log(e.target.value);
-       setComment(e.target.value)
+        setComment(e.target.value)
     }
-    const handleSubmit = () =>{
-        setCount(count+1)
-        setData([...data, {"comment" : comment,"edit":false}])
+    const handleSubmit = () => {
+        let new_id = Date.now()
+        setCount(count + 1)
+        setData([...data, {
+            "id": new_id,
+            "uname": "user"+new_id,
+            "comment": comment,
+            "edit": false,
+            "like": true,
+            "nestedComments": []
+        }])
     }
-    
+
 
     return (
-        <div style={{paddingLeft:'50px', maxWidth:'75rem', margin:'0 auto'}}>
+        <div style={{ paddingLeft: '50px', maxWidth: '75rem', margin: '0 auto' }}>
             <h1>Leave a comment</h1>
-            <div class="d-flex" style={{padding:'50px 0'}}>
-                <input type="text"  placeholder="enter comment" onChange={handleComment} />
-                <button onClick={()=>handleSubmit()}>Submit Comment</button>
+            <div class="d-flex" style={{ padding: '50px 0' }}>
+                <input type="text" className="input_section" placeholder="enter comment" onChange={handleComment} />
+                <button onClick={() => handleSubmit()}>Submit Comment</button>
             </div>
-            <button onClick={()=>setCollapse(!collapse)}>{collapse ? ("Collapse All Comments"): "Expand All Comments"}</button>
-           { collapse && <DisplayComment data={data} setData={setData} ></DisplayComment>}
+            <button onClick={() => setCollapse(!collapse)}>{collapse ? ("Collapse All Comments") : "Expand All Comments"}</button>
+            { collapse && <DisplayComment data={data} setData={setData} ></DisplayComment>}
         </div>
-        
+
     )
 }
 
