e97b748c65a3067d4e53d4020a901921572f2b69
Vam564 <64726014+Vam564@users.noreply.github.com>
2021-05-29 21:25:06 +0530

    Update DisplayComment.js

--- Finly-task/src/components/Comments/DisplayComment.js
+++ Finly-task/src/components/Comments/DisplayComment.js
@@ -6,6 +6,9 @@ const DisplayComment = ({data,setData}) => {
     const [editFlag,setEditFlag] = useState(false)
     const [isEditComment, setIsEditComment] = useState('')
     const [like, setLike] = useState(false)
+    const [isReply, setIsReply] = useState('')
+    const [replyComment, setReplyComment ] = useState('')
+    const [userName,setUserName] =useState('')
 
 console.log(data)
 const handleEditComment = (e) =>{
@@ -42,13 +45,37 @@ const handleEditedComment = (value) =>{
     
 }
 
-const handleReply = (index, value) =>{
+const handleLike = (index, value) =>{
     setData([...data.map((x,i)=>{
         if(i == index){
            x.like = !x.like
         }
         return x
     })])
+}
+const handleReplyInput = (e) =>{
+    setReplyComment(e.target.value)
+}
+const handleReplyInputSubmit = (id,index) =>{
+    let new_id = Date.now();
+    setData([...data.map((x,i)=>{
+        if(x.id == id){
+
+           x.nestedComments = [...x.nestedComments, {
+            "id": new_id,
+            "uname": "user"+new_id+" @"+userName,
+            "comment": replyComment,
+            "edit": false,
+            "like": false
+          }]
+        }
+        return x
+    })])
+    setIsReply('')
+}
+const handleReply = (id,index,userName) =>{
+    setUserName(userName)
+    setIsReply(id)
 }
     return (
         <div className="">
@@ -60,8 +87,14 @@ const handleReply = (index, value) =>{
                         <input type="text" value={isEditComment} placeholder="edit your comment" onChange={handleEditComment}/><button onClick={()=>handleEditedComment(i)}>Submit</button>
                     </>) : (
                         <>
-                            <li key={i}>{ x.comment} <span class="edit" onClick={()=>handleEdit(i,x.comment)} >Edit</span> <span class= "delete" onClick={()=>handleDelete(i,x.comment)}>Delete</span><span class="like" onClick={()=>handleReply(i,x.comment)}>{x.like ? "Liked" : "Unliked"}</span></li>  
-                             <NestedComments data={x.nestedComments} setData={setData} ></NestedComments>
+                            <small>{x.uname}</small><br></br>
+                            <li key={i}>{ x.comment} <span class="edit" onClick={()=>handleEdit(i,x.comment)} >Edit</span> <span class= "delete" onClick={()=>handleDelete(i,x.comment)}>Delete</span><span class="like" onClick={()=>handleLike(i,x.comment)}>{x.like ? "Liked" : "Unliked"}</span><span className="reply" onClick={()=>handleReply(x.id,i,x.uname)}>Reply</span></li>  
+                             <NestedComments data={data} nestedData={x.nestedComments} setData={setData} id={x.id} main_data_index={i} handleReply={handleReply}></NestedComments>
+                             {
+                                isReply == x.id && (<>
+                                    <input type="text" value={replyComment} placeholder="Enter Reply" onChange={handleReplyInput}/><button onClick={()=>handleReplyInputSubmit(x.id,i)}>Submit</button>
+                                </>)
+                             }
                              <hr></hr>
                         </>
                     )
